b2a89f6381f7fac3af29cb6d11c5db14cc835d05
LeidulfVigre <145765390+LeidulfVigre@users.noreply.github.com>
2025-04-07 15:40:41 +0200

    hei

--- app/app.py
+++ app/app.py
@@ -4,13 +4,14 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    if request.method == "GET":     
+    if request.method == "GET":
+
         return render_template("startsite_login_register.html")
     
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
         print("test")
-        return "hei"
+        return "<p>test test test</p>" 
     elif request.method == "POST":
         return "hei 2"
