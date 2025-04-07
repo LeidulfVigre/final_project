c0aee1c17d6ecc4b608cbe53c08b7291f4bddd34
Simon <40559925+Protop23@users.noreply.github.com>
2025-04-07 15:04:40 +0200

    hei

--- app/app.py
+++ app/app.py
@@ -6,7 +6,12 @@ app = Flask(__name__)
 def index():
     if request.method == "GET":     
         return render_template("startsite_login_register.html")
-    
+
+@app.route("/movie_site", methods=["GET"])
+def movie_site():
+    if request.method == "GET":
+        return render_template("movie_site.html", movie_name="Revenge of the Sith", score="5", duration="2 timer", genre="Science Fiction", age_limit="12", release_date="2001", synopsis="Bra film", country_of_origin="USA", movie_language="English")
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
